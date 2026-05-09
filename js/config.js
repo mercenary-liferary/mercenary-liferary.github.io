4634602f31f25c7ec77d39b82548e4bb0e7fd025
CB <35495739+chungboklee@users.noreply.github.com>
2026-05-09 18:55:44 +0900

    0.1.4

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.LIFERARY_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
-  DELETE_FUNCTION_URL: "",
+  SUPABASE_URL: "https://zvoifogsibxxqbgyklqe.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2b2lmb2dzaWJ4eHFiZ3lrbHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMTExODQsImV4cCI6MjA5Mzg4NzE4NH0.DpeOdtvupOfjGkaZTmaPOK5ekcVV3lNnkLaD2qQzbR8",
+  DELETE_FUNCTION_URL: "https://zvoifogsibxxqbgyklqe.supabase.co/functions/v1/delete-result",
   SITE_URL: "https://mercenary-liferary.github.io/"
 };
